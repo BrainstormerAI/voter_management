ac9b9895476ece4c0bb160a7cb83cf1e06328100
Anubhav Vaish <141219355+BrainstormerAI@users.noreply.github.com>
2025-05-23 16:32:24 +0530

    Delete voters directory

--- voters/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Voter
-
-class VoterSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Voter
-        fields = ['id', 'data']
