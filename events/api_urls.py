6706ecf23f1559ad27089d96ac2897c6d19d214a
Anubhav Vaish <141219355+BrainstormerAI@users.noreply.github.com>
2025-06-19 15:13:14 +0530

    Delete events directory

--- events/api_urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .api_views import EventDetailAPIView, EventListAPIView
-
-app_name = 'events_api'
-
-urlpatterns = [
-    path('events/', EventListAPIView.as_view(), name='event_list'),
-    path('events/<int:id>/', EventDetailAPIView.as_view(), name='event_detail'),
-] 
--- events/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'events'
-
-urlpatterns = [
-    # Admin preview URL
-    path('admin/events/<int:event_id>/preview/', views.preview_event, name='event_preview'),
-]
